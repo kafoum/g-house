51d588beb30b8745c960a42d33cfaa6db23590d5
kafou <kafoumathieu.coulibaly@gmail.com>
2025-10-02 07:34:38 +0000

    mise a jour indexjs

--- backend/index.js
+++ backend/index.js
@@ -51,7 +51,7 @@ const wss = new WebSocket.Server({ server });
 const PORT = process.env.PORT || 5000;
 
 // Connexion à la base de données MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(err => console.error('Erreur de connexion à MongoDB :', err));
 
